90ebce83aab8511371ab98d5d46aa10263bc5d12
benjaminsage <benjaminlsage@gmail.com>
2020-10-14 17:21:02 -0700

    Added faf's example to a new examples file

---
+++ Sources/iGraphics/Examples.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Benjamin Sage on 10/14/20.
+//
+
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+public extension iGraphicsView {
+    struct iGraphicShoppingApp: View {
+        public var body: some View {
+            iGraphicsBox()
+                .stack(3)
+        }
+    }
+}
