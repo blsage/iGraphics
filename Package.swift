2b0714f7fe333621365854ebdce44ea2db866405
benjaminsage <benjaminlsage@gmail.com>
2020-11-18 12:15:54 -0800

    Fixed dependencies

--- Package.swift
+++ Package.swift
@@ -7,9 +7,7 @@ let package = Package(
     name: "iGraphics",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
